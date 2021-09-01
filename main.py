c77f94cbd6314c14bd843f4b751accd0630b2644
danielnlz01 <38385574+danielnlz01@users.noreply.github.com>
2021-08-31 19:49:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+res=3*2
+print(res)
+
+hola="hola mundo"
+print(hola)
+
+hola="hola"
+nombre=" daniel. "
+resu=(hola+nombre)*5
+print(resu)
+
+#numero1=int(input("cuál es el número 1?\n"))
+#numero2=int(input("cuál es el número 2?\n"))
+#print(numero1+numero2)
+
+nombre1="danieel"
+print(len(nombre1))
+
+#numero1=str(3)
+#numero2=str(3)
+#print(numero1+numero2)
+
+#numero1=float(3)
+#numero2=float(3)
+#print(numero1+numero2)
+
+#numero1=float(input("cuál es el número 1?\n"))
+#numero2=float(input("cuál es el número 2?\n"))
+#print(numero1/numero2)
+
+print("spam"*3)
+
+a=10
+b=10.5
+print(a==b)
+
+print(6.5 - (3 % 8))
+
